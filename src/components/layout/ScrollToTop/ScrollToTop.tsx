ff247531ad400eeacb4d73264d4f4d15a778a796
mustafaDabah <mustafadabah5555@gmail.com>
2025-10-24 21:56:53 +0300

    add Small timeout to ensure navigation is complete

--- src/components/layout/ScrollToTop/ScrollToTop.tsx
+++ src/components/layout/ScrollToTop/ScrollToTop.tsx
@@ -5,11 +5,12 @@ export const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
+    // Small timeout to ensure navigation is complete
+    const timer = setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+
+    return () => clearTimeout(timer);
   }, [pathname]);
 
   return null;
